f929e2478215c0ce31308f2eaf7d714f873ad8d9
Aman Dudeja <102788999+amandudeja2134@users.noreply.github.com>
2023-08-20 14:53:11 +0530

    Update cam2_pub.py

--- cam2_pub.py
+++ cam2_pub.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
-
- 
-# Import the necessary libraries
-import rospy # Python library for ROS
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
+import rospy
+from sensor_msgs.msg import Image 
+from cv_bridge import CvBridge
+import cv2 
   
 def publish_message():
- 
-  # Node is publishing to the video_frames topic using 
-  # the message type Image
   pub = rospy.Publisher('video_frame2', Image, queue_size=10)
-     
-  # Tells rospy the name of the node.
-  # Anonymous = True makes sure the node has a unique name. Random
-  # numbers are added to the end of the name.
   rospy.init_node('video_pub_py', anonymous=True)
-     
-  # Go through the loop 10 times per second
-  rate = rospy.Rate(100) # 10hz
-     
-  # Create a VideoCapture object
-  # The argument '0' gets the default webcam.
+  rate = rospy.Rate(100)
   cam1 = cv2.VideoCapture("/dev/video2")
   cam1.set(3,640)
   cam1.set(4,360)
@@ -30,29 +15,17 @@ def publish_message():
   def resize_frame(frame, width, height):
 
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
-     
-  # Used to convert between ROS and OpenCV images
-  br = CvBridge()
+   
+  bridge = CvBridge()
  
-  # While ROS is still running.
   while not rospy.is_shutdown():
-     
-      # Capture frame-by-frame
-      # This method returns True/False as well
-      # as the video frame.
+    
       ret, frame1 = cam1.read()
-      frame1_c = resize_frame(frame1,640,360)
+      frame1_compressed = resize_frame(frame1,640,360)
          
       if ret == True:
-        # Print debugging information to the terminal
         rospy.loginfo('publishing video frame')
-             
-        # Publish the image.
-        # The 'cv2_to_imgmsg' method converts an OpenCV
-        # image to a ROS image message
-        pub.publish(br.cv2_to_imgmsg(frame1_c))
-             
-      # Sleep just enough to maintain the desired rate
+        pub.publish(bridge.cv2_to_imgmsg(frame1_compressed))
       rate.sleep()
          
 if __name__ == '__main__':
