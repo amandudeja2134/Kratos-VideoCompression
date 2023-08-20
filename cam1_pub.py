60f6dadff797b921cccb0b01f2c99fa27b7fe39c
Aman Dudeja <102788999+amandudeja2134@users.noreply.github.com>
2023-08-20 14:50:42 +0530

    Update cam1_pub.py

--- cam1_pub.py
+++ cam1_pub.py
@@ -2,53 +2,33 @@
 
  
 
-import rospy # Python library for ROS
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
+import rospy 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge 
+import cv2
   
 def publish_message():
  
   
-  pub = rospy.Publisher('video_frame1', Image, queue_size=10)
-     
-  # Tells rospy the name of the node.
-  # Anonymous = True makes sure the node has a unique name. Random
-  # numbers are added to the end of the name.
+  pub = rospy.Publisher('video_frame1', Image, queue_size=10) # publisher
   rospy.init_node('video_pub_py', anonymous=True)
-     
-  # Go through the loop 10 times per second
-  rate = rospy.Rate(40) # 10hz
-     
-  # Create a VideoCapture object
-  # The argument '0' gets the default webcam.
+  rate = rospy.Rate(40) 
   cam1 = cv2.VideoCapture("/dev/video0")
-  cam1.set(3,640)
+  cam1.set(3,640)  #decreaing resolution 
   cam1.set(4,360)
   
   def resize_frame(frame, width, height):
 
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
-     
-  # Used to convert between ROS and OpenCV images
-  br = CvBridge()
- 
-  # While ROS is still running.
+  
+  bridge = CvBridge()
   while not rospy.is_shutdown():
      
       ret, frame1 = cam1.read()
-      frame1_c = resize_frame(frame1,640,480)
+      frame1_compressed = resize_frame(frame1,640,480)
          
       if ret == True:
-        # Print debugging information to the terminal
-        rospy.loginfo('publishing video frame')
-             
-        # Publish the image.
-        # The 'cv2_to_imgmsg' method converts an OpenCV
-        # image to a ROS image message
-        pub.publish(br.cv2_to_imgmsg(frame1_c))
-             
-      # Sleep just enough to maintain the desired rate
+        pub.publish(bridge.cv2_to_imgmsg(frame1_compressed))
       rate.sleep()
          
 if __name__ == '__main__':
